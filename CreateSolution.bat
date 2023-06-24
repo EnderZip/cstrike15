c2525ddad9e90b6d9d2654aa4b369b1508093407
EnderZip <44537796+EnderZip@users.noreply.github.com>
2023-06-24 21:10:56 +0700

    initial with fixes and threaded map loading
    yes map loading is now multithreaded

---
+++ CreateSolution.bat
@@ -0,0 +1,8 @@
+devtools\bin\vpc.exe /csgo +csgo_partner +cryptlib /mksln csgo_partner.sln
+pause
+
+
+#gcsdk from kisak
+#"removed from partner depo" restoration from kisak
+#more fixes from kisak
+#basically everything is from kisak :)
